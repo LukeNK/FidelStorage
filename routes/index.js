8501c40ea9be20e81ab866381d0aa8f703cb786c
LukeNK <lukenguyenkhanh@gmail.com>
2022-06-18 19:54:07 -0700

    Temporary stuff Still need to do the API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const editJsonFile = require("edit-json-file");
+let db = {
+    storage: editJsonFile('../database/storage'),
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Fidelissma Storage System' });
+});
+
+router.get('/search', function(req, res, next) {
+    res.render('search', {title: 'Search FSS', query: 'Minecraft item, coordinate, or book'})
+});
+router.post('/search', function(req, res, next) {
+    let query = req.body.query, type = req.body.type;
+    res.render('search', {title: 'Search FSS', query: (query || 'Minecraft item, coordinate, or book')})
 });
 
 module.exports = router;
